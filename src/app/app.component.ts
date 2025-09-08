afa3fb94bfb7b49f257735dae30d50e56db9d244
Fatimahib <fatm499@hotmail.com>
2025-09-08 10:56:06 +0300

    first change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'test-ng';
+  name: string = 'Your Name';
+  age: number = 25;
+   isStudent: boolean = true;
+  skills: string[] = ['TypeScript', 'Angular', 'HTML'];
+
+    count: number = 0;
+
+  incrementCount(): void {
+      //this.count = "one"; 
+
+    this.count++;
+  }
 }
